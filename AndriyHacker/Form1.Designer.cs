7355dc71fc73c7abf49592714e0bddf838a3e75e
Eugene Filyuk <zhenyok3404@gmail.com>
2023-06-14 02:06:03 +0300

    Add project files.

---
+++ AndriyHacker/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AndriyHacker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Авторизація";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(103, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(152, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Атака по словнику";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Клієнт авторизація";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 298);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Хакер ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AndriyHacker/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace AndriyHacker
+{
+    public partial class Form1 : Form
+    {
+        private string pathToDictionary;
+        Stream stream;
+        TcpClient tcpClient;
+        public Form1()
+        {
+            InitializeComponent();
+            tcpClient = new TcpClient();
+            tcpClient.Connect("127.0.0.1", 8888);
+            stream = tcpClient.GetStream();
+            label4.Text = $"Адреса клієнта: 127.0.0.1 : {((IPEndPoint)tcpClient.Client.RemoteEndPoint).Port}";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (textBox1.Text == string.Empty)
+                {
+                    textBox1.BackColor = Color.Red;
+                    textBox1.Text = "Поле для логіну порожнє";
+                }
+                else
+                {
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        List<string> passwords = GetPasswordsFromFile(openFileDialog1.FileName);
+                        textBox2.Text = AttackByDictionary(passwords, textBox1.Text);
+                        label2.Text = "Broken password:";
+                    }
+                }
+            }
+            checkBox1.Checked = false;
+        }
+
+        List<string> GetPasswordsFromFile(string filepath)
+        {
+            return File.ReadAllLines(filepath).ToList();
+        }
+
+        string AttackByDictionary(List<string> passwords, string username)
+        {
+            for (int i = 0; i < passwords.Count; i++)
+            {
+                if (Auth(username, passwords[i]) == "Success")
+                {
+                    return passwords[i];
+                }
+            }
+
+            return "Пароль не знайдено";
+        }
+
+        string Auth(string username, string password)
+        {
+            List<byte> buffer = new List<byte>();
+            int bytesRead = 10;
+            byte[] requestData = Encoding.UTF8.GetBytes(username + " " + password + "\n");
+            stream.Write(requestData, 0 , requestData.Length);
+
+            while ((bytesRead = stream.ReadByte()) != '\n')
+            {
+                buffer.Add((byte)bytesRead);
+            }
+
+            string result = Encoding.UTF8.GetString(buffer.ToArray());
+            return result;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            byte[] request = Encoding.UTF8.GetBytes("Close" + "\n");
+            stream.Write(request,0,request.Length);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Auth(textBox1.Text, textBox2.Text) == "Success")
+            {
+                button1.BackColor = Color.Green;
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+            }
+        }
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Поле для логіну порожнє")
+            {
+                textBox1.Text = string.Empty;
+            }
+            textBox1.BackColor = Color.White;
+        }
+    }
+}
